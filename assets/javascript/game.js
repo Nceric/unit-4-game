c1ba7bf588be1a272c6e9a3f8cf2cb2186705436
Nermin Ceric <nerceric@gmail.com>
2019-08-06 23:09:51 -0500

    added buttons and tried to get them work

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+const displayScore = document.getElementById("displayScore");
+
+displayScore.innerHTML = 0;
+
+const targetScore = document.getElementById('targetScore');
+
+targetScore.innerHTML = Math.floor(math.random()* 120);
+
+
+
+
+
+
+
+
+
+const winNumber = 0;
+console.log("winNumber" + winNumber);
+
+const lossNumber = 0;
+console.log("lossNumber" + lossNumber);
+
+const score = 0;
+console.log("score:" + score);
+
+const first = addEventListener("click", function() {
+document.getElementById("black")
+}
+
+
+
+
